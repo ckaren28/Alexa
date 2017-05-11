6e3b355e63614d40bda63d0715d306e3d7bb8d3e
Karen <c.karen28@gmail.com>
2017-05-11 19:24:11 -0400

    call

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ const languageStrings = {
                 "a martini",
                 "brandy",
                 "malt liquor",
-                "wine cooler",
+                "a wine cooler or two, may I suggest Smirnoff ICE?",
                 "a dark and stormy",
                 "mulled wine",
                 "nothing"
@@ -63,9 +63,52 @@ const languageStrings = {
 
 };
 
+
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+
+//     if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
+//         context.fail("Invalid Application ID");
+//      }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+
 const handlers = {
     'LaunchRequest': function () {
-        this.emit('GetFact');
+        this.emit('Drink');
     },
     'Drink': function () {
         // this.emit(':tell','vodka');
@@ -108,3 +151,23 @@ exports.handler = (event, context) => {
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == 'TestIntent') {
+        handleTestRequest(intent, session, callback);
+    }
+    else {
+        throw "Invalid intent";
+    }
+}
